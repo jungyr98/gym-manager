ae1e88345f902a359580815b75ae22b86fb02bba
yourg <jungyr98@gmail.com>
2025-04-28 12:03:03 +0900

    주석 및 어노테이션 제거

--- src/main/java/com/potato/GymManagerBackendApplication.java
+++ src/main/java/com/potato/GymManagerBackendApplication.java
@@ -2,10 +2,8 @@ package com.potato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.potato", "com.potato.config"})
 public class GymManagerBackendApplication {
 
 	public static void main(String[] args) {
